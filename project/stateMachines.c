c5cf7d1eae0f15772f91a188546dbbfc3efed71c
Ricardo Ruvalcaba <rruvalcaba2@utep.miners.edu>
2021-04-07 00:43:16 -0600

    Binary counter and updated interrupt handler

--- project/stateMachines.c
+++ project/stateMachines.c
@@ -2,6 +2,7 @@
 #include "stateMachines.h"
 #include "led.h"
 
+
 char toggle_red()		/* always toggle! */
 {
   static char state = 0;
@@ -19,7 +20,8 @@ char toggle_red()		/* always toggle! */
   return 1;			/* always changes an led */
 }
 
-char toggle_green()	/* only toggle green if red is on!  */
+
+/*char toggle_green()    /* only toggle green if red is on!  
 {
   char changed = 0;
   if (red_on) {
@@ -28,13 +30,32 @@ char toggle_green()	/* only toggle green if red is on!  */
   }
   return changed;
 }
-
-
-void state_advance()		/* alternate between toggling red & green */
+*/
+char toggle_green()/* always toggle! */
 {
-  char changed = 0;  
+  static char state = 0;
 
+  switch (state) {
+  case 0:
+    green_on = 1;
+    state = 1;
+    break;
+
+  case 1:
+    green_on = 0;
+    state = 0;
+    break;
+  }
+  return 1;
+}
+
+
+
+void state_advance()/* alternate between toggling red & green */ 
+{
+  char changed = 0;
   static enum {R=0, G=1} color = G;
+
   switch (color) {
   case R: changed = toggle_red(); color = G; break;
   case G: changed = toggle_green(); color = R; break;
@@ -42,7 +63,24 @@ void state_advance()		/* alternate between toggling red & green */
 
   led_changed = changed;
   led_update();
+
 }
 
+void binary_count() 
+{
+  static char state = 0;
+  switch (state) {
 
+  case 0: red_on = 0; green_on = 0; break;
 
+  case 1: red_on = 1; green_on = 0; break;
+
+  case 2: red_on = 0; green_on = 1; break;
+
+  case 3: red_on = 1; green_on = 1; state = -1;
+    // switch back to case 0, since update will increment it to 0
+  }
+  state++; 
+  led_changed=1;
+  led_update();
+}
--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -4,8 +4,10 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
-  if (++blink_count == 125) {
-    state_advance();
+  if (++blink_count == 200) {
+    // state_advance();
+    //count_to_three();
+    binary_count();
     blink_count = 0;
   }
 }
