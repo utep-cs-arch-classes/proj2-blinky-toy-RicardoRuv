df914ae5f81bb476e5b491298ef1da2f2719ec15
RicardoRuv <75161937+RicardoRuv@users.noreply.github.com>
2021-04-07 13:44:06 -0600

    Update wdInterruptHandler.c
    cleaned unnecessary code

--- project/blink/wdInterruptHandler.c
+++ project/blink/wdInterruptHandler.c
@@ -6,7 +6,6 @@ __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
   if (++blink_count == 200) {
     // state_advance();
-    //count_to_three();
     binary_count();
     blink_count = 0;
   }
